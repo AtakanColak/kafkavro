e3f2a17f0a2f2bfe49aca645e5e62584640171a6
AtakanColak <atakancolak9897@gmail.com>
2020-11-12 11:55:47 +0300

    v0.0.1

--- generator/generator.go
+++ generator/generator.go
@@ -2,10 +2,99 @@ package generator
 
 import (
 	"encoding/json"
+	"fmt"
+	"reflect"
 	"strings"
+	"sync"
+	"time"
 
 	avro "github.com/actgardner/gogen-avro/v7/schema"
 )
+
+var (
+	timestampPreference      string = "timestamp-millis"
+	timestampPreferenceMutex sync.RWMutex
+
+	TimestampMillisDefinition = map[string]interface{}{"logicalType": "timestamp-millis"}
+)
+
+// AvroTypeName of a given primitive Go value
+// struct, map and arrays are not supported
+func AvroTypeName(value interface{}) (string, error) {
+	var typename string
+	err := fmt.Errorf("type %T is not supported by AvroTypeName", value)
+	if value == nil {
+		return "null", nil
+	}
+	t := reflect.TypeOf(value)
+	switch k := t.Kind(); k {
+	case reflect.Invalid:
+		typename = "null"
+	case reflect.Bool:
+		typename = "boolean"
+	case reflect.String:
+		typename = "string"
+	case reflect.Float32:
+		typename = "float"
+	case reflect.Float64:
+		typename = "double"
+	case reflect.Int8,
+		reflect.Uint8,
+		reflect.Int16,
+		reflect.Uint16,
+		reflect.Int32,
+		reflect.Int:
+		typename = "int"
+	case reflect.Uint32,
+		reflect.Int64,
+		reflect.Uint64,
+		reflect.Uint:
+		typename = "long"
+	case reflect.Slice:
+		if t.Elem().Kind() != reflect.Uint8 {
+			return "", err
+		}
+		typename = "bytes"
+	case reflect.Struct:
+		if t.PkgPath() != "time" || t.Name() != "Time" {
+			return "", err
+		}
+		typename = "long"
+	case reflect.Ptr:
+		nt := reflect.New(t.Elem())
+		if nt.IsNil() || !nt.CanInterface() {
+			return "", err
+		}
+		return AvroTypeName(nt.Elem().Interface())
+	default:
+		return "", err
+	}
+	return typename, nil
+}
+
+// ChangeTimestampPreference for time.Time values
+// Default is "timestamp-millis"
+func ChangeTimestampPreference(preference string) {
+	switch preference {
+	case "timestamp-millis", "timestamp-micros", "local-timestamp-millis", "local-timestamp-micros":
+		timestampPreferenceMutex.Lock()
+		defer timestampPreferenceMutex.Unlock()
+		timestampPreference = preference
+	default: // NO-OP if invalid preference
+	}
+}
+
+// AvroDefinition returns definition for logicalTypes
+func AvroDefinition(value interface{}) map[string]interface{} {
+	timestampPreferenceMutex.RLock()
+	defer timestampPreferenceMutex.RUnlock()
+	switch value.(type) {
+	case time.Time:
+		return map[string]interface{}{"logicalType": timestampPreference}
+	default:
+		return nil
+	}
+}
 
 // Field is a wrapper for NewField
 func Field(name, avroType string, definition map[string]interface{}) *avro.Field {
@@ -18,15 +107,28 @@ func Field(name, avroType string, definition map[string]interface{}) *avro.Field
 
 // MakeNullable is a wrapper to make an avro field nullable
 func MakeNullable(field *avro.Field) *avro.Field {
-	nullField := avro.NewNullField("null")
-	definition, _ := field.Definition(nil)
-	definition["default"] = "null"
+	bufferedField := new(avro.Field)
+	*bufferedField = *field
 
-	return avro.NewField("", avro.NewUnionField("", []avro.AvroType{nullField, field.Type()}, []interface{}{map[string]interface{}{"type1": "rr"}, map[string]interface{}{"type1": "aa"}}), nil, false, nil, "", definition, 0, "")
+	nullField := avro.NewNullField("null")
+	definition, _ := bufferedField.Definition(nil)
+
+	// if is a logicalType, recreate the Field with logicalType embedded in it because gogen-avro doesn't support it
+	if logicalTypeI, exists := definition["logicalType"]; exists {
+		logicalType, ok := logicalTypeI.(string)
+		if !ok {
+			panic("logicalType is not provided as a string")
+		}
+
+		bufferedField = Field(field.SimpleName(), `{"type":"`+definition["type"].(string)+`","logicalType":"`+logicalType+`"}`, nil)
+	}
+	definition["default"] = "null"
+	delete(definition, "logicalType")
+	return avro.NewField("", avro.NewUnionField("", []avro.AvroType{nullField, bufferedField.Type()}, []interface{}{map[string]interface{}{"type1": "rr"}, map[string]interface{}{"type1": "aa"}}), nil, false, nil, "", definition, 0, "")
 }
 
-// Record is a wrapper for NewRecord
-func Record(name, namespace string, aliases []avro.QualifiedName, fields []*avro.Field, definition map[string]interface{}) *avro.RecordDefinition {
+// RecordDefinition is a wrapper for NewRecordDefinition
+func RecordDefinition(name, namespace string, aliases []avro.QualifiedName, fields []*avro.Field, definition map[string]interface{}) *avro.RecordDefinition {
 	if definition == nil {
 		definition = make(map[string]interface{})
 	}
@@ -36,8 +138,8 @@ func Record(name, namespace string, aliases []avro.QualifiedName, fields []*avro
 	return avro.NewRecordDefinition(avro.QualifiedName{Name: name, Namespace: namespace}, aliases, fields, "", definition)
 }
 
-// SchemaFromRecord returns the schema in an ordered fashion
-func SchemaFromRecord(record *avro.RecordDefinition) (string, error) {
+// SchemaFromRecordDefinition returns the schema in an ordered fashion
+func SchemaFromRecordDefinition(record *avro.RecordDefinition) (string, error) {
 	type OrderedAvroField struct {
 		Name        string          `json:"name"`
 		Doc         string          `json:"doc,omitempty"`
@@ -122,5 +224,50 @@ func SchemaFromRecord(record *avro.RecordDefinition) (string, error) {
 		return "", err
 	}
 
+	// to avoid namespace mix
+	qualifiedName := record.AvroName()
+	os.Name = qualifiedName.Name
+	os.Namespace = qualifiedName.Namespace
+
 	return marshalAvroSchema(os), nil
+}
+
+// FieldsFromMap generates a Fields array from given map
+func FieldsFromMap(m map[string]interface{}) ([]*avro.Field, error) {
+	fields := make([]*avro.Field, 0)
+	for key, val := range m {
+		avroType, err := AvroTypeName(val)
+		if err != nil {
+			return nil, err
+		}
+		definition := AvroDefinition(val)
+		fields = append(fields, Field(key, avroType, definition))
+	}
+	return fields, nil
+}
+
+// RecordDefinitionFromMap is a wrapper utility function that generates a RecordDefinition from given map value
+// By default all fields are not nullable, for them to be nullable, you can either do it by hand or call MakeAllFieldsNullable
+func RecordDefinitionFromMap(name, namespace string, value map[string]interface{}) (*avro.RecordDefinition, error) {
+	fields, err := FieldsFromMap(value)
+	if err != nil {
+		return nil, err
+	}
+	return RecordDefinition(name, namespace, nil, fields, nil), nil
+}
+
+// MakeAllFieldsNullable for a given record, with no side effects
+func MakeAllFieldsNullable(record *avro.RecordDefinition) *avro.RecordDefinition {
+	fields := record.Fields()
+	for i := range fields {
+		fields[i] = MakeNullable(fields[i])
+	}
+
+	definitionI, _ := record.Definition(nil)
+	var definition map[string]interface{}
+	if definitionAsMap, isMap := definitionI.(map[string]interface{}); isMap {
+		definition = definitionAsMap
+	}
+
+	return avro.NewRecordDefinition(record.AvroName(), record.Aliases(), fields, record.Doc(), definition)
 }
--- generator/generator_test.go
+++ generator/generator_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/linkedin/goavro/v2"
+
 	avro "github.com/actgardner/gogen-avro/v7/schema"
 
 	"github.com/AtakanColak/kafkavro/generator"
@@ -11,8 +13,15 @@ import (
 
 // reference
 type A struct {
-	Name  string
+	Name  string `avrogen:"nullable"`
 	Value int64
+}
+
+func (a A) ToMap() (map[string]interface{}, error) {
+	return map[string]interface{}{
+		"Name":  a.Name,
+		"Value": a.Value,
+	}, nil
 }
 
 type B struct {
@@ -24,76 +33,187 @@ type B struct {
 	CPUPercent float64
 }
 
-func GenerateSchemaForTestTopicA() string {
-	record := avro.NewRecordDefinition(avro.QualifiedName{
-		Namespace: "",
-		Name:      "A",
-	},
-		nil,
-		[]*avro.Field{
-			avro.NewField("Name", avro.NewStringField("string"), nil, false, nil, "", map[string]interface{}{"name": "Name"}, 0, ""),
-			avro.NewField("Value", avro.NewLongField("long"), nil, false, nil, "", map[string]interface{}{"name": "Value"}, 0, ""),
-		},
-		"",
-		map[string]interface{}{
-			"type": "record",
-		})
-
-	s, err := record.Schema()
-	if err != nil {
-		panic(err.Error())
-	}
-	return s
+func (b B) ToMap() (map[string]interface{}, error) {
+	return map[string]interface{}{
+		"Time":       b.Time,
+		"Host":       b.Host,
+		"AgentUUID":  b.AgentUUID,
+		"MemoryFree": b.MemoryFree,
+		"PID":        b.PID,
+		"CPUPercent": b.CPUPercent,
+	}, nil
 }
 
-func recordDefinitionForB() *avro.RecordDefinition {
+var (
+	expectedSchemaA = `{"type":"record","name":"A","namespace":"com.testing","fields":[{"name":"Name","type":"string"},{"name":"Value","type":"long"}]}`
+)
+
+func checkSchema(expected, schema string, t testing.TB) {
+	if schema != expected {
+		t.Fatalf("expected:\n%s\nbut got:\n%s", expected, schema)
+	}
+
+	if _, err := goavro.NewCodec(schema); err != nil {
+		t.Fatalf("generated schema can't be parsed: %s", err.Error())
+	}
+}
+
+func TestSchemaFromRecordDefinition(t *testing.T) {
 	fields := []*avro.Field{
-		generator.Field("time", `{"type":"long","logicalType":"timestamp-millis"}`, nil),
-		generator.Field("host", "string", nil),
-		generator.Field("agent_uuid", "string", nil),
-		generator.Field("memory_free", "long", nil),
-		generator.Field("PID", "int", nil),
-		generator.Field("cpu_percent", "double", nil),
-	}
-	for i := 0; i < len(fields); i++ {
-		fields[i] = generator.MakeNullable(fields[i])
+		generator.Field("fieldA", "string", nil),
+		generator.Field("fieldB", "long", generator.TimestampMillisDefinition),
+		generator.MakeNullable(generator.Field("fieldC", "int", nil)),
 	}
 
-	return generator.Record("B", "com_test_mytest", nil, fields, nil)
-}
+	record := generator.RecordDefinition("test", "com.testing", nil, fields, nil)
+	expected := `{"type":"record","name":"test","namespace":"com.testing","fields":[{"name":"fieldA","type":"string"},{"name":"fieldB","type":"long","logicalType":"timestamp-millis"},{"name":"fieldC","type":["null","int"],"default":"null"}]}`
 
-func GenerateSchemaForTestTopicB() string {
-	record := recordDefinitionForB()
-	s, err := record.Schema()
-	if err != nil {
-		panic(err.Error())
-	}
-	return s
-}
-
-func TestGenerateSchemaForTestTopicA(t *testing.T) {
-	unorderedSchema := GenerateSchemaForTestTopicA()
-
-	t.Log(codec.Schema())
-}
-
-func TestGenerateSchemaForTestTopicB(t *testing.T) {
-	t.Log(GenerateSchemaForTestTopicB())
-}
-
-func TestSchemaFromRecord(t *testing.T) {
-	record := recordDefinitionForB()
-	s, err := generator.SchemaFromRecord(record)
+	schema, err := generator.SchemaFromRecordDefinition(record)
 	if err != nil {
 		t.Fatal(err.Error())
 	}
-	t.Log(s)
+
+	checkSchema(expected, schema, t)
 }
-func TestDefinition(t *testing.T) {
-	field := avro.NewField("time", avro.NewNullField("long"), nil, false, nil, "", map[string]interface{}{"name": "time", "logicalType": "timestamp-millis"}, 0, "")
-	def, err := field.Definition(nil)
+
+func TestSchemaFromStruct(t *testing.T) {
+	schema, err := generator.SchemaFromStruct(A{})
 	if err != nil {
 		t.Fatal(err.Error())
 	}
-	t.Log(def)
+	checkSchema(expectedSchemaA, schema, t)
 }
+
+func TestRecordDefinitionFrom(t *testing.T) {
+	a := A{
+		Name:  "Ali",
+		Value: 23,
+	}
+
+	b := B{
+		Time:       time.Now(),
+		Host:       "centos",
+		AgentUUID:  "0-jgue--d",
+		MemoryFree: 5,
+		PID:        1,
+		CPUPercent: 5.0,
+	}
+
+	_, _ = a, b
+
+	t.Run("Map", func(t *testing.T) {
+		runTestRecordDefinitionFromMapCase := func(name, namespace, expected string, m generator.Mappable) func(*testing.T) {
+			return func(t *testing.T) {
+				mapped, err := m.ToMap()
+				if err != nil {
+					t.Fatal(err.Error())
+				}
+				record, err := generator.RecordDefinitionFromMap(name, namespace, mapped)
+				if err != nil {
+					t.Fatal(err.Error())
+				}
+				schema, err := generator.SchemaFromRecordDefinition(record)
+				if err != nil {
+					t.Fatal(err.Error())
+				}
+
+				checkSchema(expected, schema, t)
+			}
+
+		}
+		t.Run("A", runTestRecordDefinitionFromMapCase("A", "com.testing", "", a))
+		t.Run("B", runTestRecordDefinitionFromMapCase("B", "com.testing", "", b))
+	})
+
+	t.Run("Mappable", func(t *testing.T) {
+		t.Skip()
+	})
+}
+
+// func TestRecordFromMappable(t *testing.T) {
+
+// 	record, err := generator.RecordDefinitionFromMappable("B", "com.test.package", b)
+// 	if err != nil {
+// 		t.Fatal(err.Error())
+// 	}
+
+// 	schema, err := generator.SchemaFromRecordDefinition(record)
+// 	if err != nil {
+// 		t.Fatal(err.Error())
+// 	}
+// 	t.Log(schema)
+// }
+
+// func GenerateSchemaForTestTopicA() string {
+// 	record := avro.NewRecordDefinition(avro.QualifiedName{
+// 		Namespace: "",
+// 		Name:      "A",
+// 	},
+// 		nil,
+// 		[]*avro.Field{
+// 			avro.NewField("Name", avro.NewStringField("string"), nil, false, nil, "", map[string]interface{}{"name": "Name"}, 0, ""),
+// 			avro.NewField("Value", avro.NewLongField("long"), nil, false, nil, "", map[string]interface{}{"name": "Value"}, 0, ""),
+// 		},
+// 		"",
+// 		map[string]interface{}{
+// 			"type": "record",
+// 		})
+
+// 	s, err := record.Schema()
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+// 	return s
+// }
+
+// func recordDefinitionForB() *avro.RecordDefinition {
+// 	fields := []*avro.Field{
+// 		generator.Field("time", "long", map[string]interface{}{"logicalType": "timestamp-millis"}),
+// 		generator.Field("host", "string", nil),
+// 		generator.Field("agent_uuid", "string", nil),
+// 		generator.Field("memory_free", "long", nil),
+// 		generator.Field("PID", "int", nil),
+// 		generator.Field("cpu_percent", "double", nil),
+// 	}
+// 	for i := 0; i < len(fields); i++ {
+// 		fields[i] = generator.MakeNullable(fields[i])
+// 	}
+
+// 	return generator.Record("B", "com_test_mytest", nil, fields, nil)
+// }
+
+// func GenerateSchemaForTestTopicB() string {
+// 	record := recordDefinitionForB()
+// 	s, err := record.Schema()
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+// 	return s
+// }
+
+// func TestGenerateSchemaForTestTopicA(t *testing.T) {
+// 	unorderedSchema := GenerateSchemaForTestTopicA()
+
+// 	t.Log(unorderedSchema)
+// }
+
+// func TestGenerateSchemaForTestTopicB(t *testing.T) {
+// 	t.Log(GenerateSchemaForTestTopicB())
+// }
+
+// func TestSchemaFromRecord(t *testing.T) {
+// 	record := recordDefinitionForB()
+// 	s, err := generator.SchemaFromRecord(record)
+// 	if err != nil {
+// 		t.Fatal(err.Error())
+// 	}
+// 	t.Log(s)
+// }
+// func TestDefinition(t *testing.T) {
+// 	field := avro.NewField("time", avro.NewNullField("long"), nil, false, nil, "", map[string]interface{}{"name": "time", "logicalType": "timestamp-millis"}, 0, "")
+// 	def, err := field.Definition(nil)
+// 	if err != nil {
+// 		t.Fatal(err.Error())
+// 	}
+// 	t.Log(def)
+// }
